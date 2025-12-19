7434f4b55abc0fac2beeefec91290723f0066a56
lydiagroza <131545909+lydiagroza@users.noreply.github.com>
2025-12-19 23:40:11 +0200

    coomit 2

--- src/main/java/org/example/MidiHello.java
+++ src/main/java/org/example/MidiHello.java
@@ -20,7 +20,20 @@ public class MidiHello {
 
             instrument.programChange(50);// trumpet for now
 
-            int[] majorScale = {60, 62, 64, 65, 67, 69, 71, 72};
+            int[] majorScale = {
+                    60, 60, 60, // Root
+                    62,
+                    64, 64,     // Third
+                    65,
+                    67, 67,     // Fifth
+                    69,
+                    71,
+                    72
+            };
+
+            int[] rythms = {
+                    250, 250, 250, 500, 500, 1000
+            };
 
 
            System.out.println("Starting loop...");
@@ -28,16 +41,21 @@ public class MidiHello {
            for(int i = 0; i < 20; i++){
 
                int randomIndex = random.nextInt(majorScale.length);
-
                int note =  majorScale[randomIndex];
+
+               int rythmIndex = random.nextInt(rythms.length);
+                int rythm = rythms[rythmIndex];
 
                System.out.println("Playing note: " + note);
 
+
                instrument.noteOn(note, 90);
 
-               Thread.sleep(500);
+               Thread.sleep((int)(rythm * 0.9));
 
                instrument.noteOff(note);
+
+               Thread.sleep((int)(rythm * 0.1));
            }
 
             System.out.println("Done loop");
